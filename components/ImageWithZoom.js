2a72495880c42e56f1b18756d3334a7ee50443ac
Jekack <lachehol@gmail.com>
2025-04-09 17:05:17 +0300

    Update ImageWithZoom.js

--- components/ImageWithZoom.js
+++ components/ImageWithZoom.js
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import React from 'react';
 
 interface ImageWithZoomProps {
@@ -11,12 +10,12 @@ interface ImageWithZoomProps {
 
 const ImageWithZoom: React.FC<ImageWithZoomProps> = ({ image }) => {
   return (
-    <div className="flex-1 flex items-center justify-center p-4">
-      <div className="relative w-full max-w-4xl max-h-[80vh] overflow-hidden group">
+    <div className="flex items-center justify-center h-full p-4">
+      <div className="relative w-full max-w-3xl max-h-[70vh] overflow-hidden group flex items-center justify-center">
         <img
           src={image.src}
           alt={image.title}
-          className="w-full h-full object-contain transform transition-transform duration-500 group-hover:scale-105 rounded-xl"
+          className="w-auto h-full max-h-[70vh] object-contain transition-transform duration-500 group-hover:scale-105 rounded-xl"
         />
       </div>
     </div>
